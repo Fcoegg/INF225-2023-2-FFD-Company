2ac6f01b295686ec416cc2d913e377394cb75840
Fcoegg <119830289+Fcoegg@users.noreply.github.com>
2023-10-26 13:01:51 -0300

    Add files via upload

---
+++ database/db.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE ProyectoSoft;
+
+CREATE TABLE ingreso(
+    id SERIAL PRIMARY KEY,
+    inspector VARCHAR(60),
+    fecha DATE,
+    lugar VARCHAR(60),
+    patente VARCHAR(6),
+    infraccion VARCHAR(60),
+    empadronado BOOLEAN ,
+    notificacion BYTEA
+);
